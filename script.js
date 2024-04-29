8658a27eb06da3f281e15e2dedfa81583043994a
Portia-Nelly-Mashaba <pnpnelly@gmail.com>
2024-04-29 23:19:08 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector(".navbar-nav");
+const navTogglebtn = document.querySelector(".nav-toggle-btn");
+const navLinks = document.querySelectorAll(".nav-link");
+//navbar toggle function
+const navToggleFunc = function(){
+    nav.classList.toggle('active');
+    navTogglebtn.classList.toggle('active');
+}
+navTogglebtn.addEventListener('click', navToggleFunc);
+
+for (let i=0; i< navLinks.length; i++){
+    navLinks(i).addEventListener('click', navToggleFunc);
+}
